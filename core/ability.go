ddedfd53cd70e6c232921779fbcf6b3ebf478a10
Keegan Brown <keeganbrown@outlook.de>
2026-05-09 16:33:08 +0200

    more polished Ability

--- core/ability.go
+++ core/ability.go
@@ -1,63 +1,34 @@
 package core
 
-import "fmt"
+// Ability represents a single Ability score (STR, DEX, etc.)
+//
+// An Ability Score consists of a BaseScore, as well as any number of Bonuses, like:
+//
+// - Character Creation Ability Point distributions
+// - Feats
+// - Temporary Buffs or Debuffs
+type Ability struct {
+	SupportsBonuses
+	BaseScore int
+}
 
-type Ability int
+func NewAbility(baseScore int) *Ability {
+	return &Ability{
+		SupportsBonuses: NewSimpleBonusSupporter(),
+		BaseScore:       baseScore,
+	}
+}
 
-func (a Ability) BonusModifier() int {
+func (a *Ability) Score() int {
+	return a.BaseScore + a.ComputeBonuses()
+}
+
+func (a *Ability) Modifier() int {
 	// offset from 10, divided by 2, rounded down
-	offset := int(a) - 10
+	score := a.Score()
+	offset := int(score) - 10
 	if offset >= 0 {
 		return offset / 2
 	}
 	return (offset - 1) / 2
 }
-
-// Abilities holds the six ability scores for a character.
-type Abilities struct {
-	Strength     Ability
-	Dexterity    Ability
-	Constitution Ability
-	Wisdom       Ability
-	Intelligence Ability
-	Charisma     Ability
-}
-
-func (a *Abilities) GetAbility(kind AbilityKind) Ability {
-	switch kind {
-
-	case AbilityKindStrength:
-		return a.Strength
-
-	case AbilityKindDexterity:
-		return a.Dexterity
-
-	case AbilityKindConstitution:
-		return a.Constitution
-
-	case AbilityKindWisdom:
-		return a.Wisdom
-
-	case AbilityKindIntelligence:
-		return a.Intelligence
-
-	case AbilityKindCharisma:
-		return a.Charisma
-
-	default:
-		msg := "unknown ability kind: %v"
-		panic(fmt.Sprintf(msg, kind))
-	}
-}
-
-type AbilityKind int
-
-const (
-	AbilityKindStrength     AbilityKind = 0x1
-	AbilityKindDexterity    AbilityKind = 0x2
-	AbilityKindConstitution AbilityKind = 0x3
-	AbilityKindWisdom       AbilityKind = 0x4
-	AbilityKindIntelligence AbilityKind = 0x5
-	AbilityKindCharisma     AbilityKind = 0x6
-)
-
